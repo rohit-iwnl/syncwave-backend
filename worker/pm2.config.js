0823bbdc4748161aa05a7f94fbda815490b30856
rohit-iwnl <rohitmanivel9@gmail.com>
2024-10-29 15:18:59 -0700

    add pm2 runtime config

---
+++ worker/pm2.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [{
+    name: "syncwave-worker",
+    script: "./src/index.ts",
+    interpreter: "bun",
+    env: {
+      NODE_ENV: "production",
+      PATH: `${process.env.HOME}/.bun/bin:${process.env.PATH}`,
+    },
+    max_memory_restart: '1G',
+    autorestart: true,
+    exec_mode: 'fork',
+    instances: 1,
+    kill_timeout: 3000,
+    wait_ready: true
+  }]
+}
