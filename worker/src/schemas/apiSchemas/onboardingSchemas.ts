46bc807f74950f71bc7666bfe8865ef3ae91db4b
rohit-iwnl <rohitmanivel9@gmail.com>
2024-10-29 19:56:20 -0700

    add setPrefences zod obejct

--- worker/src/schemas/apiSchemas/onboardingSchemas.ts
+++ worker/src/schemas/apiSchemas/onboardingSchemas.ts
@@ -3,6 +3,13 @@ import { z } from 'zod';
 const onboardingSchemas = Object.freeze({
     createUser : z.object({
         name : z.string().min(4)
+    }),
+    
+    setPreferences : z.object({
+        lease_sublease_property : z.boolean(),
+        find_roomate : z.boolean(),
+        sell_buy_product : z.boolean(),
+        here_to_explore : z.boolean()
     })
 });
 
