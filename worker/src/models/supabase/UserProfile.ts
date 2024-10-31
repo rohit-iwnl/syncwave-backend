90badc9bb0ef55833b78dd383cd021d57c52caf7
rohit-iwnl <rohitmanivel9@gmail.com>
2024-10-31 15:11:01 -0700

    update user profile model

--- worker/src/models/supabase/UserProfile.ts
+++ worker/src/models/supabase/UserProfile.ts
@@ -1,63 +1,94 @@
-import mongoose from 'mongoose'
+import mongoose from "mongoose";
 
 const userProfileSchema = new mongoose.Schema({
     supabase_id: {
         type: String,
         required: true,
-        unique: true
+        unique: true,
     },
     email: {
         type: String,
-        required: true
+        required: true,
     },
     phone: {
         type: String,
-        default: null
+        default: null,
     },
     last_sign_in_at: {
         type: Date,
-        default: null
+        default: null,
     },
     created_at: {
         type: Date,
-        default: Date.now
+        default: Date.now,
     },
     updated_at: {
         type: Date,
-        required: true
+        required: true,
     },
     deleted_at: {
         type: Date,
-        default: null
+        default: null,
     },
     preferences: {
         type: {
             find_roommate: {
                 type: Boolean,
                 required: true,
-                default: false
+                default: false,
             },
             here_to_explore: {
                 type: Boolean,
                 required: true,
-                default: false
+                default: false,
             },
             lease_property: {
                 type: Boolean,
                 required: true,
-                default: false
+                default: false,
             },
             sell_buy_product: {
                 type: Boolean,
                 required: true,
-                default: false
-            }
+                default: false,
+            },
         },
         required: false,
-        default: null
-    }
-})
+        default: null,
+    },
+    personal_details: {
+        type : {
+            country: {
+                type: String,
+                required: false,
+                default: null,
+            },
+            state: {
+                type: String,
+                required: false,
+                default: null,
+            },
+            gender: {
+                type: String,
+                required: false,
+                default: null,
+            },
+            field: {
+                type: String,
+                required: false,
+                default: null,
+            },
+            pronouns: {
+                type: String,
+                required: false,
+                default: null,
+            },
+        },
+        required: false,
+        default: null,
+    },
+});
 
-const UserModel = mongoose.model('UserProfile', userProfileSchema)
+const UserModel = mongoose.model("UserProfile", userProfileSchema);
 
-export default UserModel
+export default UserModel;
