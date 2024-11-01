48643fad0ac475dab43594fd459d2e673ce3aed0
rohit-iwnl <rohitmanivel9@gmail.com>
2024-10-31 20:12:53 -0700

    prettier format

--- worker/pm2.config.js
+++ worker/pm2.config.js
@@ -1,28 +1,25 @@
 module.exports = {
-  apps: [{
-    name: "syncwave-worker",
-    script: "./src/index.ts",
-    interpreter: "bun",
-    env: {
-      NODE_ENV: "production",
-      PATH: `${process.env.HOME}/.bun/bin:${process.env.PATH}`,
+  apps: [
+    {
+      name: "syncwave-worker",
+      script: "./src/index.ts",
+      interpreter: "bun",
+      env: {
+        NODE_ENV: "production",
+        PATH: `${process.env.HOME}/.bun/bin:${process.env.PATH}`,
+      },
+      max_memory_restart: "1G",
+      autorestart: true,
+      exec_mode: "fork",
+      instances: 1,
+      kill_timeout: 3000,
+      wait_ready: true,
+      watch: true,
+      ignore_watch: ["node_modules", "logs", ".git", "*.log"],
+      watch_options: {
+        followSymlinks: false,
+        usePolling: true,
+      },
     },
-    max_memory_restart: '1G',
-    autorestart: true,
-    exec_mode: 'fork',
-    instances: 1,
-    kill_timeout: 3000,
-    wait_ready: true,
-    watch: true,
-    ignore_watch: [
-      "node_modules",
-      "logs",
-      ".git",
-      "*.log"
-    ],
-    watch_options: {
-      followSymlinks: false,
-      usePolling: true
-    }
-  }]
-}
+  ],
+};
