8299aca195fe503bdc040db970ffcd0ae2403c64
rohit-iwnl <rohitmanivel9@gmail.com>
2024-11-01 15:57:07 -0700

    vim formatting lol idk waht to change now

--- worker/src/routes/supabaseSync.ts
+++ worker/src/routes/supabaseSync.ts
@@ -1,46 +1,46 @@
 import { Hono } from "hono";
 import {
-    handleUserCreated,
-    handleUserDeleted,
-    handleUserUpdated,
-} from "@/utils/userSync";
+  handleUserCreated,
+  handleUserDeleted,
+  handleUserUpdated,
+} from "@/utils/userUtils";
 
 const supabaseSyncRouter = new Hono();
 
 supabaseSyncRouter.get("/", (c) => {
-    return c.json({
-        message: "Supabase sync",
-    });
+  return c.json({
+    message: "Supabase sync",
+  });
 });
 
 supabaseSyncRouter.post("/sync/user", async (c) => {
-    const body = await c.req.json();
-    const { type, record } = body;
+  const body = await c.req.json();
+  const { type, record } = body;
 
-    switch (type) {
-        case "INSERT":
-            await handleUserCreated(record);
-            break;
-        case "UPDATE":
-            await handleUserUpdated(record);
-            break;
-        case "DELETE":
-            await handleUserDeleted(record);
-            break;
-        default:
-            return c.json({ error: `Unsupported webhook type: ${type}` }, 400);
-    }
+  switch (type) {
+    case "INSERT":
+      await handleUserCreated(record);
+      break;
+    case "UPDATE":
+      await handleUserUpdated(record);
+      break;
+    case "DELETE":
+      await handleUserDeleted(record);
+      break;
+    default:
+      return c.json({ error: `Unsupported webhook type: ${type}` }, 400);
+  }
 
-    return c.json({ success: true }, 200);
+  return c.json({ success: true }, 200);
 });
 
 supabaseSyncRouter.post("/sync/user/view", async (c) => {
-    const body = await c.req.json();
-    const { type, record } = body;
-    return c.json({
-        type,
-        record,
-    });
+  const body = await c.req.json();
+  const { type, record } = body;
+  return c.json({
+    type,
+    record,
+  });
 });
 
 export default supabaseSyncRouter;
