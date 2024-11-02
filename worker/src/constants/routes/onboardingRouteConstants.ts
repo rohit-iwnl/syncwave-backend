08d1ead88f55dc49f04113b007e071f52bfd222d
rohit-iwnl <rohitmanivel9@gmail.com>
2024-11-01 22:37:21 -0700

    add api routes as constants

--- worker/src/constants/routes/onboardingRouteConstants.ts
+++ worker/src/constants/routes/onboardingRouteConstants.ts
@@ -1,5 +1,10 @@
 const onboardingRouteConstants = Object.freeze({
     BASE_PATH: "/api/onboarding",
+    SET_PREFERENCES: "/set-preferences",
+    SET_FIND_ROOM_PREFERENCES: "/set-find-room-preferences",
+    SET_HOUSING_PREFERENCES: "/set-housing-preferences",
+    SET_PERSONAL_DETAILS: "/set-personal-details",
+
 })
 
 export default onboardingRouteConstants;
