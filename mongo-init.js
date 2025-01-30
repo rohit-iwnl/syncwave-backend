d32d10515010eea77568eb6dbf6aa7492a4e4c44
rohit-iwnl <rohitmanivel9@gmail.com>
2025-01-29 20:14:28 -0700

    add auth to mongoku

--- mongo-init.js
+++ mongo-init.js
@@ -6,9 +6,7 @@ db.createUser({
   user: process.env.MONGO_INITDB_ROOT_USERNAME,
   pwd: process.env.MONGO_INITDB_ROOT_PASSWORD,
   roles: [
-    {
-      role: "readWrite",
-      db: process.env.MONGO_INITDB_DATABASE
-    }
+    { role: "readWrite", db: process.env.MONGO_INITDB_DATABASE },
+    { role: "dbAdmin", db: process.env.MONGO_INITDB_DATABASE }
   ]
 }); 
