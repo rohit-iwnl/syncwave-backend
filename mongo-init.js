ad2617f14727d882fab61b41c2f290a8ac8f994a
rohit-iwnl <rohitmanivel9@gmail.com>
2025-01-29 21:05:51 -0700

    Update mongo-init.js to use Bun.env for environment variables

--- mongo-init.js
+++ mongo-init.js
@@ -1,16 +1,16 @@
 // mongo-init.js
 db.getSiblingDB('admin').auth(
-  process.env.MONGO_INITDB_ROOT_USERNAME,
-  process.env.MONGO_INITDB_ROOT_PASSWORD
+  Bun.env(MONGO_INITDB_ROOT_USERNAME),
+  Bun.env(MONGO_INITDB_ROOT_PASSWOR)
 );
 
-db = db.getSiblingDB(process.env.MONGO_INITDB_DATABASE);
+db = db.getSiblingDB(Bun.env(MONGO_INITDB_DATABASE));
 
 db.createUser({
-  user: process.env.MONGO_INITDB_ROOT_USERNAME,
-  pwd: process.env.MONGO_INITDB_ROOT_PASSWORD,
+  user: Bun.env(MONGO_INITDB_ROOT_USERNAME),
+  pwd: Bun.env(MONGO_INITDB_ROOT_PASSWORD),
   roles: [
-    { role: "dbOwner", db: process.env.MONGO_INITDB_DATABASE },
+    { role: "dbOwner", db: Bun.env(MONGO_INITDB_DATABASE)},
     { role: "clusterAdmin", db: "admin" },
     { role: "readWrite", db: "config" }
   ]
