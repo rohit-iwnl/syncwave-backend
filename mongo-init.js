b3b70ebeffc3797a99ffff3336c9b9036b97f77f
rohit-iwnl <rohitmanivel9@gmail.com>
2025-01-29 20:43:53 -0700

    add new role permissions in mongo init

--- mongo-init.js
+++ mongo-init.js
@@ -1,18 +1,17 @@
-// Use admin database for initial authentication
+// mongo-init.js
 db.getSiblingDB('admin').auth(
   process.env.MONGO_INITDB_ROOT_USERNAME,
   process.env.MONGO_INITDB_ROOT_PASSWORD
 );
 
-// Create or switch to application database
 db = db.getSiblingDB(process.env.MONGO_INITDB_DATABASE);
 
-// Create application user with proper privileges
 db.createUser({
   user: process.env.MONGO_INITDB_ROOT_USERNAME,
   pwd: process.env.MONGO_INITDB_ROOT_PASSWORD,
   roles: [
     { role: "dbOwner", db: process.env.MONGO_INITDB_DATABASE },
-    { role: "clusterAdmin", db: "admin" }
+    { role: "clusterAdmin", db: "admin" },
+    { role: "readWrite", db: "config" }
   ]
 });
